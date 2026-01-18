e5b6267f0b39a15127319f80d2069d66fba96a7c
maynor-lopez <maynor94l39@gmail.com>
2026-01-18 17:08:49 -0600

    Adding focus state and improving modal interactivity

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const menuCloseIcon = document.getElementById("menu-close-icon");
 const hideMenu = document.getElementById("hide-menu");
 const body = document.getElementById("body");
 
-menuIcon.addEventListener("click", () => {
+menuIcon.addEventListener("click", (e) => {
   hideMenu.classList.add("show__menu");
   body.classList.add("overlay-activo");
 });
 
-menuCloseIcon.addEventListener("click", () => {
+menuCloseIcon.addEventListener("click", (e) => {
   hideMenu.classList.remove("show__menu");
   body.classList.remove("overlay-activo");
 });
+
+document.addEventListener("click", (e) => {
+  if (
+    !menuIcon.contains(e.target) &&
+    !hideMenu.contains(e.target)
+  ) {
+    hideMenu.classList.remove("show__menu");
+    body.classList.remove("overlay-activo");
+  }
+});
+
